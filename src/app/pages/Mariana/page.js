1938cc7f1698d358fa7c2ae426db008083b56063
Mariana <m.castaneda@ccbantigua.edu.gt>
2023-09-20 08:21:06 -0600

    cambios Mariana Página props

--- src/app/pages/Mariana/page.js
+++ src/app/pages/Mariana/page.js
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import {Header, PaletaDeColores, Filap } from "./componentes/componentes";
+import {Header, PaletaDeColores, Filap, EstructuraFila } from "./componentes/componentes";
 
 export default function Home() {
   return (
     <div>
       <Header></Header>
-      <Filap></Filap>
-      <Filap></Filap>
       <Filap></Filap>
     </div>
   );

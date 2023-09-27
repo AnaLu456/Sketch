cbc13d0c60a9f32c71820935fea5aba39757a7fa
ALICIA TOLEDO <ca.toledo@ccbantigua.edu.gt>
2023-09-27 13:27:38 -0600

    cambios ultimos ahora si

--- src/app/packages/company/Componentes/header.js
+++ src/app/packages/company/Componentes/header.js
@@ -3,11 +3,15 @@ import React, { useState } from 'react';
 import styles from '../Componentes/header.module.css';
 import Image from 'next/image';
 import img1 from '../imagenes/Wave.png'
+import Link from 'next/link';
 
 function Header() {
     return (
         <div className={styles.Header}>
-            <Image src={img1} className={styles.logo}></Image>
+            <Link target='_self' href="/">
+                <Image src={img1} className={styles.logo}></Image>
+
+            </Link>
         </div>
     );
 }
@@ -15,12 +19,12 @@ function Header() {
 function PaletaDeColores() {
     return (
         <div className={styles.paleta}>
-                <div className={styles.color1}>#000000</div>
-                <div className={styles.color2}>#F3EEE1</div>
-                <div className={styles.color3}>#EC5E32</div>
-                <div className={styles.color4}>#F3F1A2</div>
+            <div className={styles.color1}>#000000</div>
+            <div className={styles.color2}>#F3EEE1</div>
+            <div className={styles.color3}>#EC5E32</div>
+            <div className={styles.color4}>#F3F1A2</div>
         </div>
     );
-} 
+}
 
-export {Header,PaletaDeColores};
+export { Header, PaletaDeColores };

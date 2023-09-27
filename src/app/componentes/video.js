f47ee0e2391e27dbc8c8a1ec3a54756c1306b40f
Marco Díaz <ma.diaz@ccbantigua.edu.gt>
2023-09-27 08:55:51 -0600

    cambio de video

--- src/app/componentes/video.js
+++ src/app/componentes/video.js
@@ -5,7 +5,7 @@ import styles from "./video.module.css"
 function Video() {
   return (
     <div className={styles.video}>
-      <iframe className={styles.contenido} src="https://www.youtube.com/embed/ytTw4KZnop8?si=J6GzZ0FBuxhNi3DR" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <iframe className={styles.contenido} src="https://www.youtube.com/embed/mUxzKVrSAjs?si=ZRt2Quw70jGoPn-o" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   );
 }

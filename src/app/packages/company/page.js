fa978624cdaa0d09511471562cfa13790cf1604a
ALICIA TOLEDO <ca.toledo@ccbantigua.edu.gt>
2023-09-29 11:31:16 -0600

    CAMBIO ULTIMO

--- src/app/packages/company/page.js
+++ src/app/packages/company/page.js
@@ -17,7 +17,7 @@ export default function Home() {
            12 historias mensuales Organización de highlights Pro Copyright Monthly Analytics Client Service. " clase={styles.texto} precioD="585" precioQ="4,095" paquete="MANTAIN" />
       <EstructuraFila account={"COMPANY ACCOUNT"} imagenes={img2} texto="2 sesión del fotos al mes, 20 post mensuales en el feed, (16 fotos y 4 reels) 20 historias mensuales + organización de highlights Pro Copyright Monthly Analytics Client Service Adds Consultancies." clase={styles.texto} precioD="978" precioQ="6,850" paquete="IDEAL" />
 
-      <EstructuraFila account={"COMPANY ACCOUNT"} imagenes={img3} texto="2 sesión del fotos al mes, 20 post mensuales en el feed, (12 fotos y 8 reels) 28 historias al mes + organización de highlights Pro Copyright Monthly Analytics Client Service Adds Consultancies." clase={styles.texto} precioD="1,170" precioQ="8,190" paquete="MANTAIN" />
+      <EstructuraFila account={"COMPANY ACCOUNT"} imagenes={img3} texto="2 sesión del fotos al mes, 20 post mensuales en el feed, (12 fotos y 8 reels) 28 historias al mes + organización de highlights Pro Copyright Monthly Analytics Client Service Adds Consultancies." clase={styles.texto} precioD="1,170" precioQ="8,190" paquete="LIVE" />
 
       <ContactPage></ContactPage>
 
